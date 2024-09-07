46888cbca9818c3c92ec533f8d2eed4b366be0be
katredford <katharine.redford@gmail.com>
2024-09-07 15:21:59 -0500

    fix keyboard rows

--- src/components/keyboard/KeyBoard.tsx
+++ src/components/keyboard/KeyBoard.tsx
@@ -1,39 +1,63 @@
-
-import Keys from './Keys'
-import letters from './characters.json'
-import './keyboard.css'
-
+import React from 'react';
+import Keys from './Keys';
+import letters from './characters.json';
+import './keyboard.css';
 
 interface KeyBoardProps {
     keyClick: (char: string) => void;
-
 }
 
 const KeyBoard: React.FC<KeyBoardProps> = ({ keyClick }) => {
 
+    const rowOne = letters.slice(0, 10);     
+    const rowTwo = letters.slice(10, 19);   
+    const rowThree = letters.slice(19, 28);  
 
     return (
         <>
             <div className="keybox">
                 <div className='lettersBox'>
+                    <div className="row keyRow">
+                        {rowOne.map((letter) => (
+                            <Keys
+                                key={letter.id}
+                                char={letter.letter}
+                                onClick={keyClick}
+                                className="keys"
+                            />
+                        ))}
+                    </div>
 
-                    {letters.map((letter) => (
+                    <div className="row keyRow">
+                        {rowTwo.map((letter) => (
+                            <Keys
+                                key={letter.id}
+                                char={letter.letter}
+                                onClick={keyClick}
+                                className="keys"
+                            />
+                        ))}
+                    </div>
 
-                        <Keys
-                            key={letter.id}
-                            char={letter.letter}
-                            onClick={keyClick}
-                            className={letter.letter === "P" || letter.letter === "L" ? "key break-after" : "keys"}
-                        />
-                    ))}
+                    <div className="row keyRow">
+                        {rowThree.map((letter) => (
+                            <Keys
+                                key={letter.id}
+                                char={letter.letter}
+                                onClick={keyClick}
+                                className="keys"
+                            />
+                        ))}
+                    </div>
                 </div>
-
             </div>
         </>
-    )
+    );
 };
 
-
-
-
 export default KeyBoard;
+
+
+
+
+
